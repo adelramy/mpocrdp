388d5a4fbe1f8d637bd1c4fc6f6d915f9faed901
adelramy <adelramy9015@gmail.com>
2025-02-24 20:07:49 -0800

    Update show.bat

--- show.bat
+++ show.bat
@@ -5,10 +5,10 @@ python -c "import pyautogui as pag; pag.click(155, 554, duration=2)"
 python -c "import pyautogui as pag; pag.click(637, 417, duration=2)"
 python -c "import pyautogui as pag; pag.click(588, 10, duration=2)"
 
-echo Script By (Jor3a Ti9niya) :
+echo Script By (me) :
 
 echo "Unlock endless RDP access for free! Follow us on social media and discover the power of limitless possibilities!"
 
-echo Blog : https://technical-dose.blogspot.com/
+echo Blog : -----------------------------
 
-echo Youtube : https://www.youtube.com/@Jor3a-Ti9niya
+echo Youtube : ------------------------------
