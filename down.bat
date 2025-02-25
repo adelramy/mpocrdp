2df94d6827446e035e341c9b7201fbe83fdf4fd0
adelramy <adelramy9015@gmail.com>
2025-02-24 20:09:04 -0800

    Update down.bat

--- down.bat
+++ down.bat
@@ -7,9 +7,7 @@ powershell -Command "Expand-Archive -Path 'litemanager.zip' -DestinationPath '%c
 pip install pyautogui --quiet
 choco install vcredist-all --no-progress
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
-del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
-del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
-net user runneradmin Jor3a_Ti9niya
+net user runneradmin me
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 start "" "LiteManager Pro - Server.msi"
 python setup.py
